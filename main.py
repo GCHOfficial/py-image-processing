addf5bbd3f1585ae55871803c78b0eb1cf480be9
George-Cosmin Hanta <gchofficial@gmail.com>
2022-03-24 23:29:42 +0200

    Version 0.1.1

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
         super(PyGUI, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.statusbar.setFont(QtGui.QFont("Sans Serif", 8))
         self.ui.pushButton.clicked.connect(self.handle_browse_button)
         self.ui.pushButton_2.clicked.connect(self.handle_convert_button)
 
@@ -24,9 +23,6 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
         else:
             if self.ui.radioButton.isChecked():
                 self.filenames.clear()
-                self.ui.statusbar.showMessage(
-                    "JPG to PNG selected. Select input files location"
-                )
                 dlg = QtWidgets.QFileDialog()
                 dlg.setFileMode(QtWidgets.QFileDialog.ExistingFiles)
                 dlg.setNameFilters(["Images (*.jpg)"])
@@ -36,9 +32,6 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.handle_ui_checkboxes()
             elif self.ui.radioButton_2.isChecked():
                 self.filenames.clear()
-                self.ui.statusbar.showMessage(
-                    "PNG to JPG selected. Select input files location"
-                )
                 dlg = QtWidgets.QFileDialog()
                 dlg.setFileMode(QtWidgets.QFileDialog.ExistingFiles)
                 dlg.setNameFilters(["Images (*.png)"])
@@ -48,9 +41,6 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.handle_ui_checkboxes()
             elif self.ui.radioButton_3.isChecked():
                 self.filenames.clear()
-                self.ui.statusbar.showMessage(
-                    "GIF creation selected. Select input files location"
-                )
                 dlg = QtWidgets.QFileDialog()
                 dlg.setFileMode(QtWidgets.QFileDialog.ExistingFiles)
                 dlg.setNameFilters(["Images (*.png *.jpg)"])
@@ -59,9 +49,11 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.filenames.append(dlg.selectedFiles())
                     self.handle_ui_checkboxes()
             else:
-                self.ui.statusbar.showMessage(
-                    "Please select an action using the radio buttons"
-                )
+                msg = QtWidgets.QMessageBox()
+                msg.setIcon(2)
+                msg.setText("Processing mode has to be selected before input files!")
+                msg.setWindowTitle("Missing processing")
+                msg.exec_()
 
     def handle_convert_button(self):
         if (
@@ -70,9 +62,19 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
             or self.ui.radioButton_3.isChecked()
         ):
             if self.filenames == []:
-                self.ui.statusbar.showMessage("Load input files first.")
+                msg = QtWidgets.QMessageBox()
+                msg.setIcon(2)
+                msg.setText("No input files were selected for processing")
+                msg.setWindowTitle("Missing input")
+                msg.exec_()
             else:
                 self.image_conversion()
+        else:
+            msg = QtWidgets.QMessageBox()
+            msg.setIcon(2)
+            msg.setText("First select processing mode and input files")
+            msg.setWindowTitle("Missing processing")
+            msg.exec_()
 
     def handle_ui_checkboxes(self):
         if self.filenames == []:
@@ -85,7 +87,7 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
             self.ui.radioButton.setEnabled(False)
             self.ui.radioButton_2.setEnabled(False)
             self.ui.radioButton_3.setEnabled(False)
-    
+
     def handle_finish(self):
         self.filenames.clear()
         self.handle_ui_checkboxes()
@@ -99,7 +101,7 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
     def success_message_box(self):
         msg = QtWidgets.QMessageBox()
         msg.setIcon(1)
-        msg.setText("Image(s) converted succesfully!")
+        msg.setText("Image(s) processed succesfully!")
         msg.setWindowTitle("Success!")
         msg.exec_()
 
@@ -110,38 +112,41 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
             if self.ui.radioButton.isChecked():
                 dlg.setNameFilters(["Images (*.png)"])
                 dlg.selectNameFilter("Images (*.png)")
+                path = []
                 if dlg.exec_():
                     path = dlg.selectedFiles()
-                for file in self.filenames[0]:
-                    image = Image.open(file)
-                    imagepath = (
-                        path[0]
-                        + re.search(r"[\/|\\]([^/\\]+)[\..+]", file).group()
-                        + "png"
-                    )
-                    image.save(imagepath)
-                self.ui.statusbar.showMessage("Image conversion finished!")
-                self.success_message_box()
-                self.handle_finish()
+                if path != []:
+                    for file in self.filenames[0]:
+                        image = Image.open(file)
+                        imagepath = (
+                            path[0]
+                            + re.search(r"[\/|\\]([^/\\]+)[\..+]", file).group()
+                            + "png"
+                        )
+                        image.save(imagepath)
+                    self.success_message_box()
+                    self.handle_finish()
             if self.ui.radioButton_2.isChecked():
                 dlg.setNameFilters(["Images (*.png)"])
                 dlg.selectNameFilter("Images (*.png)")
+                path = []
                 if dlg.exec_():
                     path = dlg.selectedFiles()
-                for file in self.filenames[0]:
-                    image = Image.open(file)
-                    imagepath = (
-                        path[0]
-                        + re.search(r"[\/|\\]([^/\\]+)[\..+]", file).group()
-                        + "jpg"
-                    )
-                    image.save(imagepath)
-                self.ui.statusbar.showMessage("Image conversion finished!")
-                self.success_message_box()
-                self.handle_finish()
+                if path != []:
+                    for file in self.filenames[0]:
+                        image = Image.open(file)
+                        imagepath = (
+                            path[0]
+                            + re.search(r"[\/|\\]([^/\\]+)[\..+]", file).group()
+                            + "jpg"
+                        )
+                        image.save(imagepath)
+                    self.success_message_box()
+                    self.handle_finish()
             elif self.ui.radioButton_3.isChecked():
                 dlg.setNameFilters(["Images (*.gif)"])
                 dlg.selectNameFilter("Images (*.gif)")
+                path = []
                 if dlg.exec_():
                     path = dlg.selectedFiles()
                 images = []
@@ -155,29 +160,26 @@ class PyGUI(QtWidgets.QMainWindow, Ui_MainWindow):
                         images[counter] = images[counter].resize(imgsize)
                     counter += 1
                 imagepath = path[0] + self.os_check_for_path() + "result.gif"
-                images[0].save(
-                    imagepath,
-                    format="GIF",
-                    save_all=True,
-                    append_images=images,
-                    optimize=False,
-                    duration=300,
-                    loop=0,
-                )
-                self.ui.statusbar.showMessage("Image conversion finished!")
-                self.success_message_box()
-                self.handle_finish()
+                if path != []:
+                    images[0].save(
+                        imagepath,
+                        format="GIF",
+                        save_all=True,
+                        append_images=images[1:],
+                        optimize=False,
+                        duration=self.ui.spinBox.value(),
+                        loop=not self.ui.checkBox.isChecked(),
+                    )
+                    self.success_message_box()
+                    self.handle_finish()
         except UnidentifiedImageError as err:
             message = str(err).split("'")[1]
             msg = QtWidgets.QMessageBox()
             msg.setIcon(3)
-            msg.setText("The following image could not be converted:")
+            msg.setText("The following image could not be processed:")
             msg.setInformativeText(message)
-            msg.setWindowTitle("Unidentified image error")
+            msg.setWindowTitle("Unidentified image")
             msg.exec_()
-            self.ui.statusbar.showMessage(
-                "Unidentified image in input. Try using a different one!"
-            )
 
 
 def main():
